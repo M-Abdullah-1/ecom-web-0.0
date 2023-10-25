331298d003ee71a576281bddd3e4fd9e0b6ad848
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-10-25 11:59:26 +0500

    make sidebar responsive

--- src/layouts/admin/header/Header.tsx
+++ src/layouts/admin/header/Header.tsx
@@ -1,21 +1,55 @@
-import { Avatar, Badge } from "antd";
+import { Avatar, Badge, Button, Drawer, Space } from "antd";
 import profilePic from "./../../../assets/profilePic.jpg";
 import style from "./style.module.scss";
-import { BellOutlined, MessageOutlined } from "@ant-design/icons";
+import { BellOutlined, MenuOutlined, MessageOutlined } from "@ant-design/icons";
+import type { DrawerProps } from "antd/es/drawer";
+import { useState } from "react";
+import SideBar from "../sidebar/SideBar";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const [size, setSize] = useState<DrawerProps["size"]>();
+
+  const showDefaultDrawer = () => {
+    setSize("default");
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={style.main}>
-      <Badge color="lime" dot>
-        <MessageOutlined className={style.icon} />
-      </Badge>
-      <BellOutlined className={style.icon} />
-      <Avatar
-        className={style.avatar}
-        size={40}
-        src={profilePic}
-        draggable={true}
-      />
+      <div className={style.left} onClick={showDefaultDrawer}>
+        <MenuOutlined />
+      </div>
+      <div className={style.right}>
+        <Badge color="lime" dot>
+          <MessageOutlined className={style.icon} />
+        </Badge>
+        <BellOutlined className={style.icon} />
+        <Avatar
+          className={style.avatar}
+          size={40}
+          src={profilePic}
+          draggable={true}
+        />
+      </div>
+      <Drawer
+        // title={`${size} Drawer`}
+        placement="left"
+        size={size}
+        width={250}
+        onClose={onClose}
+        open={open}
+        style={{
+          background: "rgba(255,255,255,0.6)",
+          backdropFilter: "blur(20px)",
+        }}
+      >
+        <SideBar />
+      </Drawer>
     </div>
   );
 };
