7bba9e5254af8fe8f4361ec90159beb75ad138b9
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-10-27 17:52:19 +0500

    add featured product section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,32 @@
+import React, { useState } from "react";
 import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
 import style from "./style.module.scss";
-import { Avatar } from "antd";
+import { Avatar, Drawer } from "antd";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={style.container}>
       <h3>Ecom</h3>
       <div className={style.rightSection}>
         <div className={style.icon}>
-          <ShoppingCartOutlined />
+          <div onClick={showDrawer}>
+            <ShoppingCartOutlined />
+          </div>
+          <Drawer title="Cart" placement="right" onClose={onClose} open={open}>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </Drawer>
         </div>
         <Avatar size="small" icon={<UserOutlined />} />
       </div>
--- src/pages/user/Home/index.tsx
+++ src/pages/user/Home/index.tsx
@@ -5,6 +5,11 @@ import pic0 from "./../../../assets/product/images00.jpeg";
 import pic1 from "./../../../assets/product/images01.jpeg";
 import pic2 from "./../../../assets/product/images02.jpeg";
 import pic3 from "./../../../assets/product/images03.jpeg";
+import pic4 from "./../../../assets/product/images04.jpeg";
+import pic5 from "./../../../assets/product/images05.jpeg";
+import pic6 from "./../../../assets/product/images06.jpeg";
+import pic7 from "./../../../assets/product/images07.jpeg";
+import pic8 from "./../../../assets/product/images08.jpeg";
 
 const productData = [
   {
@@ -33,6 +38,39 @@ const productData = [
   },
 ];
 
+const featuredProductData = [
+  {
+    id: `${Math.floor(Math.random() * 100) + 1}`,
+    title: "Lorem ipsum dolor sit...",
+    price: 43,
+    pic: pic4,
+  },
+  {
+    id: `${Math.floor(Math.random() * 100) + 1}`,
+    title: "Ipsum sit...",
+    price: 89,
+    pic: pic5,
+  },
+  {
+    id: `${Math.floor(Math.random() * 100) + 1}`,
+    title: "Lorem ipsum...",
+    price: 40,
+    pic: pic6,
+  },
+  {
+    id: `${Math.floor(Math.random() * 100) + 1}`,
+    title: "Sitipsum dolor...",
+    price: 23,
+    pic: pic7,
+  },
+  {
+    id: `${Math.floor(Math.random() * 100) + 1}`,
+    title: "Sitipsum dolor...",
+    price: 23,
+    pic: pic8,
+  },
+];
+
 const Index = () => {
   return (
     <div className={style.container}>
@@ -47,6 +85,39 @@ const Index = () => {
         </div>
         <div className={style.productRow}>
           {productData.map((el) => (
+            <div className={style.box} key={el.id}>
+              <div className={style.pic}>
+                <Image
+                  style={{
+                    borderTopLeftRadius: "10px",
+                    borderTopRightRadius: "10px",
+                  }}
+                  src={el.pic}
+                />
+              </div>
+              <div className={style.infoSection}>
+                <div className={style.title}>
+                  <span>{el.title}</span>
+                </div>
+                <div className={style.footer}>
+                  <div className={style.icon}>
+                    <HeartOutlined />
+                  </div>
+                  <div className={style.price}>
+                    <span>${el.price}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={style.productSection}>
+        <div className={style.sectionTitle}>
+          <h1>Featured Products</h1>
+        </div>
+        <div className={style.productRow}>
+          {featuredProductData.map((el) => (
             <div className={style.box} key={el.id}>
               <div className={style.pic}>
                 <Image
