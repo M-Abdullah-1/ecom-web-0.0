cdb63f55507454f141f609c1c3c8a18c309b2bcc
Muhammad Abdullah <mabdullahg196@gmail.com>
2023-10-28 12:04:36 +0500

    create checkout box on cart page

--- src/pages/user/cart/index.tsx
+++ src/pages/user/cart/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "antd";
 import CartTable from "../../../components/cartTable";
 import style from "./style.module.scss";
 
@@ -7,6 +8,33 @@ const index = () => {
       <div className={style.header}>Your Items in Cart</div>
       <div className={style.cartProductSection}>
         <CartTable />
+        <div className={style.checkoutBox}>
+          <div className={style.title}>
+            <h3>Summary</h3>
+          </div>
+          <div className={style.row}>
+            <span>Subtotal</span>
+            <span>$90</span>
+          </div>
+          <div className={style.row}>
+            <span>Estimated Shipping & Handling</span>
+            <span>$12</span>
+          </div>
+          <div className={style.row}>
+            <span>Estimated Tax</span>
+            <span>--</span>
+          </div>
+
+          <div className={style.row}>
+            <span>Total</span>
+            <span>$123</span>
+          </div>
+          <div className={style.btnBox}>
+            <Button type="primary" block>
+              Checkout
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
